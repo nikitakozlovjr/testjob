d4580d64adf4932f980bf7c53dc4b71c25ba6bf2
nikitakozlovjr <nikita3726898@gmail.com>
2024-05-08 16:27:48 +0300

    [FIX] change logil

--- task1/index.js
+++ task1/index.js
@@ -1,19 +1,19 @@
 const mapping = {
-    '0': 'компьютеров',
-    '1': 'компьютер',
-    '2': 'компьютера',
-    '3': 'компьютера',
-    '4': 'компьютера',
-    '5': 'компьютеров',
-    '6': 'компьютеров',
-    '7': 'компьютеров',
-    '8': 'компьютеров',
-    '9': 'компьютеров'
+    0: 'компьютеров',
+    1: 'компьютер',
+    2: 'компьютера',
+    3: 'компьютера',
+    4: 'компьютера',
+    5: 'компьютеров',
+    6: 'компьютеров',
+    7: 'компьютеров',
+    8: 'компьютеров',
+    9: 'компьютеров'
 };
 
 const getWordCombination = (count) => {
-    const lastNum = String(count)[String(count).length - 1];
-    return `${count} ${mapping[lastNum]}`;
+    const remainderDivision = count % 10;
+    return `${count} ${mapping[remainderDivision]}`;
 };
 
 export default getWordCombination;
